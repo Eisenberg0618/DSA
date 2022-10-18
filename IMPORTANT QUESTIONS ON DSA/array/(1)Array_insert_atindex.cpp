4b25eb6f1136d4b8debb47a2c8c729fca36d1d43
Eisenberg0618 <100901899+Eisenberg0618@users.noreply.github.com>
2022-10-18 16:18:15 +0530

    Delete IMPORTANT QUESTIONS ON DSA/array directory

--- IMPORTANT QUESTIONS ON DSA/array/(1)Array_insert_atindex.cpp
+++
@@ -1,9 +0,0 @@
-// insert at index
-#include <bits/stdc++.h>
-using namespace std;
-void insertatindex(int arr[], int n, int idx, int x)
-{
-    for (int i = n - 1; i >= idx; i--)
-        arr[i + 1] = arr[i];
-    arr[idx] = x; // time O(n) space(1)
-}
